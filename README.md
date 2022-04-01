41d6c733073b0e2097bdddec2ccf725ce97f022d
Amirreza Jananfar <amirrezajananfar@gmail.com>
2022-04-01 20:29:18 +0430

    Some part of program has been changed and updated

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ A simple online map which is programmed in procedural programming
 4. jQuery
 5. Ajax
 6. Bootstrap
+
+* Program language is Persian.
+* Secret information to login in admin panel is just a simple 'admin' which is, it can be changed in the config.php file.
+* This program is simple now, but it will be better as time elapses and its codes will refactor to object-oriented programming.
+* To use this program it would be better to put the whole data in a folder which is named 'map' or change the codes are in the 'constants.php' file.
+* Name your database 'map' then import the 'locations.sql' file into your database or if you want to change the database name, change database configs in the 'configs.php' file.
+* This program needs a package which is named 'Verta' to change the Gregorian date to the Persian date so before using please install the Verta package by the 'composer' with the 'composer.json file.
---
+++ database/locations.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 01, 2022 at 05:05 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `map`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
+
+CREATE TABLE `locations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `title` varchar(512) NOT NULL,
+  `lat` double NOT NULL,
+  `lng` double NOT NULL,
+  `type` tinyint(3) UNSIGNED NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `lat` (`lat`),
+  ADD UNIQUE KEY `lng` (`lng`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
